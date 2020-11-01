1896c6e6f0c2e82990f7815d0f8efacede7f24db
DavidYu <yuyang545262477@gmail.com>
2020-11-01 20:12:28 +0800

    finish the challenge.

--- LagestTimeForGivenDigits/code.test.ts
+++ LagestTimeForGivenDigits/code.test.ts
@@ -1,18 +1,20 @@
 import {largestTimeFromDigits} from "./code";
 
-test("23:14", () => {
+test("23:41", () => {
     expect(largestTimeFromDigits([1, 2, 3, 4])).toBe("23:41");
 });
-
 test("return empty", () => {
     expect(largestTimeFromDigits([5, 5, 5, 5])).toBe("");
 });
 test("return 00:00", () => {
     expect(largestTimeFromDigits([0, 0, 0, 0])).toBe("00:00");
 });
+
 test("return 10:00", () => {
     expect(largestTimeFromDigits([0, 0, 1, 0])).toBe("10:00");
 });
-
+test("return 21:00", () => {
+    expect(largestTimeFromDigits([0, 0, 1, 2])).toBe("21:00");
+});
 
 
--- LagestTimeForGivenDigits/code.ts
+++ LagestTimeForGivenDigits/code.ts
@@ -1,3 +1,52 @@
-export function largestTimeFromDigits(arr: number[]) {
-    return "";
-}
+export const largestTimeFromDigits = (arr: number[]) => {
+    const fullSortArray = (arr: number[], backArray: Array<number[]>) => {
+        if (arr.length < 2) {
+            backArray.push(arr);
+            return backArray;
+        }
+        for (let i = 0; i < arr.length; i++) {
+            const item = arr[i];
+            const cloneArray = [...arr.slice(0, i), ...arr.slice(i + 1)];
+            const nestArrays = fullSortArray(cloneArray, []);
+            nestArrays.forEach(_ => {
+                for (let j = 0; j < arr.length; j++) {
+                    const distArray = [..._.slice(0, j), item, ..._.slice(j)];
+                    backArray.push(distArray);
+                }
+            });
+        }
+
+        return backArray;
+    };
+
+    //排列可能性
+    const items = fullSortArray(arr, []);
+    console.table(items);
+
+    items.push(arr);
+    const distItems = items
+        .filter((distArray) => {
+            return ((distArray[0] * 10 + distArray[1]) < 24)
+                && ((distArray[2] * 10 + distArray[3]) < 60);
+        });
+    if (distItems.length <= 0) {
+        // 排除所有,不合适时间规则的元素
+        return "";
+    }
+    distItems.sort((a, b) => {
+        return (
+            a[0] * 10 * 60 +
+            a[1] * 60 +
+            a[2] * 10 +
+            a[3]) - (
+            b[0] * 10 * 60 +
+            b[1] * 60 +
+            b[2] * 10 +
+            b[3]
+        );
+    });
+
+    console.table(distItems);
+    const distItem = distItems[distItems.length - 1];
+    return `${distItem[0]}${distItem[1]}:${distItem[2]}${distItem[3]}`;
+};
